1b46bb0e7a62e8e7543710fcca9e985a4773373d
Jan Möbius <moebius@cs.rwth-aachen.de>
2019-04-09 13:22:18 +0200

    Added a documentation hint. closes #65

--- src/OpenMesh/Tools/Subdivider/Uniform/LoopT.hh
+++ src/OpenMesh/Tools/Subdivider/Uniform/LoopT.hh
@@ -363,8 +363,11 @@ private: // topological modifiers
 
     _m.set_face_handle( new_heh, _m.face_handle(heh) );
     _m.set_halfedge_handle( vh, new_heh);
+
+    // We cant reconnect a non existing face, so we skip this here if necessary
     if ( !_m.is_boundary(heh) )
       _m.set_halfedge_handle( _m.face_handle(heh), heh );
+
     _m.set_halfedge_handle( vh1, opp_new_heh );
 
     // Never forget this, when playing with the topology
