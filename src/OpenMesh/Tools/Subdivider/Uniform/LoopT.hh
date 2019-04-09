752c8ef7d23b0538dc2ee1594203add180ae8ac8
Jan Möbius <moebius@cs.rwth-aachen.de>
2019-04-09 13:27:03 +0200

    Fixed loop subdivider with external vector type

--- src/OpenMesh/Tools/Subdivider/Uniform/LoopT.hh
+++ src/OpenMesh/Tools/Subdivider/Uniform/LoopT.hh
@@ -321,7 +321,7 @@ private: // topological modifiers
     typename mesh_t::VertexHandle   vh1(_m.to_vertex_handle(heh));
     typename mesh_t::Point          midP(_m.point(_m.to_vertex_handle(heh)));
     midP += _m.point(_m.to_vertex_handle(opp_heh));
-    midP *= static_cast<typename mesh_t::Point::value_type>(0.5);
+    midP *= 0.5;
 
     // new vertex
     vh                = _m.new_vertex( midP );
@@ -393,7 +393,7 @@ private: // geometry helper
     // boundary edge: just average vertex positions
     if (_m.is_boundary(_eh) )
     {
-      pos *= static_cast<typename MeshType::Point::value_type>(0.5);
+      pos *= 0.5;
     }
     else // inner edge: add neighbouring Vertices to sum
     {
