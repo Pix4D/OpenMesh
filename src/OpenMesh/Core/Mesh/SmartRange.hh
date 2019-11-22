2b8a59e2e1093ab88c440b47f1a901d3adbf653b
Max Lyon <lyon@cs.rwth-aachen.de>
2019-11-22 11:02:31 +0100

    add method that converts smart range to set

--- src/OpenMesh/Core/Mesh/SmartRange.hh
+++ src/OpenMesh/Core/Mesh/SmartRange.hh
@@ -45,6 +45,7 @@
 #include <utility>
 #include <array>
 #include <vector>
+#include <set>
 
 //== NAMESPACES ===============================================================
 
@@ -186,6 +187,23 @@ struct SmartRangeT
     std::vector<typename std::remove_reference<decltype (f(std::declval<HandleT>()))>::type> res;
     for (const auto& e : *range)
       res.push_back(f(e));
+    return res;
+  }
+
+  /** @brief Convert range to set.
+  *
+  * Converts the range of elements into a set of objects returned by functor \p f.
+  *
+  *  @param f Functor that is applied to all elements before putting them into the set. If no functor is provided
+  *           the set will contain the handles.
+  */
+  template <typename Functor = Identity>
+  auto to_set(Functor&& f = {}) -> std::set<typename std::remove_reference<decltype (f(std::declval<HandleT>()))>::type>
+  {
+    auto range = static_cast<const RangeT*>(this);
+    std::set<typename std::remove_reference<decltype (f(std::declval<HandleT>()))>::type> res;
+    for (const auto& e : *range)
+      res.insert(f(e));
     return res;
   }
 
