d1a2794808e89c1bb3a2569578c23adf95c79132
Jan Möbius <moebius@cs.rwth-aachen.de>
2020-04-22 08:07:00 +0200

    Switch to new policy for 3.17 cmake

--- cmake/ACGQt.cmake
+++ cmake/ACGQt.cmake
@@ -101,5 +101,12 @@ macro (acg_qt5)
     # Enable automoc
     set(CMAKE_AUTOMOC ON)
 
+    if(${CMAKE_VERSION} VERSION_GREATER "3.17") 
+
+      cmake_policy(SET CMP0100 NEW)
+      message("Version greater 3.17, enable policy")
+    endif()
+
+
   endif (QT5_FOUND)
 endmacro ()
