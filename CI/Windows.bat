4bbdbd36074ca83ab949bc8deca7b1c991d70ca9
Jan Möbius <moebius@cs.rwth-aachen.de>
2021-02-01 09:36:33 +0100

    Switched Qt Version for Windows

--- CI/Windows.bat
+++ CI/Windows.bat
@@ -56,8 +56,8 @@ IF "%BUILD_PLATFORM%" == "VS2019" (
     set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.com"
 
     IF "%ARCHITECTURE%" == "x64" (
-      set QT_INSTALL_PATH=E:\Qt\Qt5.15.0\5.15.0\msvc2019_64
-      set QT_BASE_CONFIG=-DQT5_INSTALL_PATH=E:\Qt\Qt5.15.0\5.15.0\msvc2019_64
+      set QT_INSTALL_PATH=E:\Qt\5.15.2\msvc2019_64
+      set QT_BASE_CONFIG=-DQT5_INSTALL_PATH=E:\Qt\5.15.2\msvc2019_64
     )
 
 )
