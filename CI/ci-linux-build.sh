316137d3eaed59d29bf071dabe38b13a2a0a0d16
Johannes Lenzen <johannes.lenzen@rwth-aachen.de>
2021-02-09 10:35:05 +0100

    Added debug output

--- CI/ci-linux-build.sh
+++ CI/ci-linux-build.sh
@@ -63,7 +63,7 @@ else
   fi
 
   echo "Copying all required libraries of OpenMesh to the systemlib directory"
-  ls -la
+  ls -laR Build
   ldd Build/lib/libOpenMeshCored.so.9.0 | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -v '{}' systemlib
 fi
 
